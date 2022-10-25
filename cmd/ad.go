f6aaef45b446518885ea47ee3c19535ad7dc938c
Brian Dwyer <brian.dwyer@broadridge.com>
2022-10-25 16:50:14 -0400

    Update for function deprecation

--- cmd/ad.go
+++ cmd/ad.go
@@ -31,7 +31,7 @@ var adSignedInUserCmd = &cobra.Command{
 	Use:       "signed-in-user",
 	Short:     "Show graph information about current signed-in user in CLI.",
 	ValidArgs: []string{"show"},
-	Args:      cobra.ExactValidArgs(1),
+	Args:      cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
 	Run: func(cmd *cobra.Command, args []string) {
 		switch args[0] {
 		case "show":
